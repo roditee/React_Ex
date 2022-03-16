7e0aabc26dd2d531ca580c16d5354470a17f6601
roditee <roditee@naver.com>
2022-03-16 10:41:27 +0900

    initial Upload

---
+++ ex/axios-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AxiosTest from './components/AxiosTest';
+
+function App() {
+  return (
+    <div className="App">
+      <AxiosTest></AxiosTest>
+    </div>
+  );
+}
+
+export default App;
---
+++ ex/axios-app/src/components/AxiosTest.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function AxiosTest() {
+    // state
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // 서버에 요청해서 데이터 받아옴
+    // state 값 지정
+    const loadData = async() => {
+        setLoading(true);
+        const response = await axios.get('http://jsonplaceholder.typicode.com/todos/1');
+        console.log(response.data);
+        setData(response.data);
+        setLoading(false);
+    }
+
+    // 렌더링할때마다 호출
+    // 빈 배열 : loadData
+    useEffect( () => {
+        loadData();
+    }, []);
+
+
+    // data를 JSON 오브젝트로 변환해서 key, value 추출
+    const jsonObj = JSON.parse(JSON.stringify(data));
+    let result = [];
+    for (var key in jsonObj) {
+        // if(jsonObj[key] === null)
+        result.push(
+            <tr><td>{key}</td><td>{jsonObj[key]}</td></tr>
+        )
+    }
+
+
+    return (
+        <div>
+            <h3>서버로부터 받아온 값</h3>
+            <table border="1">
+                {result}
+            </table>
+        </div>
+    );
+}
+
+export default AxiosTest;
